fefdc995fe8d66130829e3e04255a41d3bb1075d
iabdinur <iabdinur1@gmail.com>
2023-06-04 03:15:06 +0100

    Blueprint and Configuration setup

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,11 +13,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = 'True'
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.main.routes import main
+from flaskblog.posts.routes import posts
+from flaskblog.users.routes import users
+
+app.register_blueprint(main)
+app.register_blueprint(posts)
+app.register_blueprint(users)
